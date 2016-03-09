02ae3971d3dbe63c1ff6cf1c1a9b35ce255c0509
Carlos Ferreira <carlosferreira@outlook.com>
2016-03-09 17:03:49 +0000

    Code Refactoring and fixing of SVG renderer not taking into account alpha values

---
+++ classes/MinecraftSkinRenderer.php
@@ -0,0 +1,6 @@
+<?php
+
+class MinecraftSkinRenderer
+{
+    const SECONDS_TO_CACHE = 604800; // Cache for 7 days
+}
---
+++ classes/Point.php
@@ -0,0 +1,91 @@
+<?php
+
+/**
+ * Class Point
+ */
+class Point
+{
+    private $_originCoord;
+    private $_destCoord = array();
+    private $_isProjected = false;
+    private $_isPreProjected = false;
+
+    function __construct($originCoord)
+    {
+        if (is_array($originCoord) && count($originCoord) == 3)
+        {
+            $this->_originCoord = array(
+                'x' => (isset($originCoord['x']) ? $originCoord['x'] : 0),
+                'y' => (isset($originCoord['y']) ? $originCoord['y'] : 0),
+                'z' => (isset($originCoord['z']) ? $originCoord['z'] : 0)
+            );
+        }
+        else
+        {
+            $this->_originCoord = array(
+                'x' => 0,
+                'y' => 0,
+                'z' => 0
+            );
+        }
+    }
+
+    function project()
+    {
+        global $cos_alpha, $sin_alpha, $cos_omega, $sin_omega;
+        global $minX, $maxX, $minY, $maxY;
+        // 1, 0, 1, 0
+        $x = $this->_originCoord['x'];
+        $y = $this->_originCoord['y'];
+        $z = $this->_originCoord['z'];
+        $this->_destCoord['x'] = $x * $cos_omega + $z * $sin_omega;
+        $this->_destCoord['y'] = $x * $sin_alpha * $sin_omega + $y * $cos_alpha - $z * $sin_alpha * $cos_omega;
+        $this->_destCoord['z'] = -$x * $cos_alpha * $sin_omega + $y * $sin_alpha + $z * $cos_alpha * $cos_omega;
+        $this->_isProjected = true;
+        $minX = min($minX, $this->_destCoord['x']);
+        $maxX = max($maxX, $this->_destCoord['x']);
+        $minY = min($minY, $this->_destCoord['y']);
+        $maxY = max($maxY, $this->_destCoord['y']);
+    }
+
+    function preProject($dx, $dy, $dz, $cos_alpha, $sin_alpha, $cos_omega, $sin_omega)
+    {
+        if (!$this->_isPreProjected)
+        {
+            $x = $this->_originCoord['x'] - $dx;
+            $y = $this->_originCoord['y'] - $dy;
+            $z = $this->_originCoord['z'] - $dz;
+            $this->_originCoord['x'] = $x * $cos_omega + $z * $sin_omega + $dx;
+            $this->_originCoord['y'] = $x * $sin_alpha * $sin_omega + $y * $cos_alpha - $z * $sin_alpha * $cos_omega + $dy;
+            $this->_originCoord['z'] = -$x * $cos_alpha * $sin_omega + $y * $sin_alpha + $z * $cos_alpha * $cos_omega + $dz;
+            $this->_isPreProjected = true;
+        }
+    }
+
+    /**
+     * @return array
+     */
+    function getOriginCoord()
+    {
+        return $this->_originCoord;
+    }
+
+    function getDestCoord()
+    {
+        return $this->_destCoord;
+    }
+
+    function getDepth()
+    {
+        if (!$this->_isProjected)
+        {
+            $this->project();
+        }
+        return $this->_destCoord['z'];
+    }
+
+    function isProjected()
+    {
+        return $this->_isProjected;
+    }
+}
